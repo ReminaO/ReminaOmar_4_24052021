036d08b457eb982c6d715552b116cd25e54a2ede
ReminaO <oremina@hotmail.fr>
2021-06-07 18:33:34 +0200

    Suppression JS non utilisé

--- js/bootstrap.js
+++
@@ -1,5 +0,0 @@
-/*!
- * Bootstrap v3.3.5 (http://getbootstrap.com)
- * Copyright 2011-2015 Twitter, Inc.
- * Licensed under the MIT license
- */
--- js/jquery-2.1.0.js
+++
@@ -1,2 +0,0 @@
-/*! jQuery v2.1.0 | (c) 2005, 2014 jQuery Foundation, Inc. | jquery.org/license */ 
-
